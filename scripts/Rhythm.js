9228153623cc18ff5484dfb264e38caabc3cadaa
Andy Jenkins <andy.jenkins@rhythmagency.com>
2016-11-01 12:06:54 -0700

    Fixed some bugs with baseline code

--- scripts/Rhythm.js
+++ scripts/Rhythm.js
@@ -1,6 +1,9 @@
 var Rhythm = {};
 var extend = require('util')._extend;
 
+/*  BuildUrl
+ *  Combines a base url string with an object containing query parameters, and returns the resulting url as a string.
+ */
 Rhythm.BuildUrl = function (base, query) {
     var result = base;
     if (query) {
@@ -13,6 +16,9 @@ Rhythm.BuildUrl = function (base, query) {
     return result;
 }
 
+/*  GetStyleObject
+ *  Converts a style string into an object. Exits immediately if source parameter is already an object.
+ */
 Rhythm.GetStyleObject = function (source) {
     if (typeof source === 'object') return source;
 
@@ -26,21 +32,41 @@ Rhythm.GetStyleObject = function (source) {
     return result;
 }
 
-Rhythm.MergeAttributes = function (base, extension) {
-    base = base ? base : {};
-    extension = extension ? extension : {};
-    if (base.style && extension.style) {
-        base.style = Rhythm.GetStyleObject(base.style);
-        extension.style = Rhythm.GetStyleObject(extension.style);
-        extension.style = extend(base.style, extension.style);
-    }
-    return extend(base, extension);
+/*  ImplyStyles
+ *  Checks a tag parameters object to make sure that the styles sub-object exists and is an object. Also populates alternate style properties.
+ */
+Rhythm.ImplyStyles = function (params) {
+    params.style = params.style ? Rhythm.GetStyleObject(params.style) : {}
+
+    if (params.width)
+        params.style.width = params.width + (/^\d+$/.test(params.width) ? "px" : "");
+        
+    if (params.align)
+        params.style["text-align"] = params.align;
+
+    return params;
 }
 
-Rhythm.AppendStyle = function (base, extension) {
+/*  ExtendStyle
+ *  Extends a style object with another one. Calls GetStyleObject first to handle string inputs.
+ */
+Rhythm.ExtendStyle = function (base, extension) {
     base = Rhythm.GetStyleObject(base ? base : {});
     extension = Rhythm.GetStyleObject(extension ? extension : {});
     return extend(base, extension);
+}
+
+/*  ExtendAttributes
+ *  Extends a tag parameters object with another one. Also calls ImplyStyles on each parameter before combining them, and merges the resulting styles objects.
+ */
+Rhythm.ExtendAttributes = function (base, extension) {
+    base = Rhythm.ImplyStyles(base ? base : {});
+    extension = Rhythm.ImplyStyles(extension ? extension : {});
+
+    extension.style = Rhythm.ExtendStyle(base.style, extension.style);
+    var result = extend(base, extension);
+
+    return result;
 }
 
 module.exports = Rhythm;
