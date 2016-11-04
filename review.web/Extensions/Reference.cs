4cf5411ee64d6a484cc5415f08cf315372b7d1d2
Andy Jenkins <andy.jenkins@rhythmagency.com>
2016-11-04 10:03:25 -0700

    Cleaned up structure, parameterized text

--- review.web/Extensions/Reference.cs
+++ review.web/Extensions/Reference.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
 namespace review.web {
     public static class Reference {
 
-        public static string ApplicationName { get { return "Rhythm Email Preview"; } }
+        public static string CompanyName {
+            get {
+                return
+                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:CompanyName"]) ?
+                        "Rhythm" :
+                        ConfigurationManager.AppSettings["Reference:CompanyName"];
+            }
+        }
+
+        public static string ApplicationName {
+            get {
+                return
+                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:ApplicationName"]) ?
+                        "Rhythm Email Preview" :
+                        ConfigurationManager.AppSettings["Reference:ApplicationName"];
+            }
+        }
 
     }
 }
