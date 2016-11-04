cc6eaa57dce0792ef7e7b648c6d0522fcd5554e6
Andy Jenkins <andy.jenkins@rhythmagency.com>
2016-11-04 09:56:20 -0700

    Added favicon stack

---
+++ review.web/Extensions/Reference.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace review.web {
+    public static class Reference {
+
+        public static string ApplicationName { get { return "Rhythm Email Preview"; } }
+
+    }
+}
