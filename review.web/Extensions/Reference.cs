dd55c3fa117aaf9fd418987e9513e8fd3c8fcdda
Andy Jenkins <andy.jenkins@rhythmagency.com>
2016-11-10 10:16:59 -0800

    Reformatted Reference.cs

--- review.web/Extensions/Reference.cs
+++ review.web/Extensions/Reference.cs
@@ -7,58 +7,32 @@ using System.Web;
 namespace review.web {
     public static class Reference {
 
+        private static string GetFromConfigOrDefault(string key, string defaultValue) {
+            return String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings[key]) ? defaultValue : ConfigurationManager.AppSettings[key];
+        }
+
         public static string CompanyName {
-            get {
-                return
-                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:CompanyName"]) ?
-                        "{ Company Name }" :
-                        ConfigurationManager.AppSettings["Reference:CompanyName"];
-            }
+            get { return GetFromConfigOrDefault("Reference:CompanyName", "{ Company Name }"); }
         }
 
         public static string ApplicationName {
-            get {
-                return
-                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:ApplicationName"]) ?
-                        "{ Application Name }" :
-                        ConfigurationManager.AppSettings["Reference:ApplicationName"];
-            }
+            get { return GetFromConfigOrDefault("Reference:ApplicationName", "{ Application Name }"); }
         }
 
         public static string ClientName {
-            get {
-                return
-                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:ClientName"]) ?
-                        "{ Client Name }" :
-                        ConfigurationManager.AppSettings["Reference:ClientName"];
-            }
+            get { return GetFromConfigOrDefault("Reference:ClientName", "{ Client Name }"); }
         }
 
         public static string MailSenderName {
-            get {
-                return
-                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:MailSenderName"]) ?
-                        "{ Mail Sender Name }" :
-                        ConfigurationManager.AppSettings["Reference:MailSenderName"];
-            }
+            get { return GetFromConfigOrDefault("Reference:MailSenderName", "{ Mail Sender Name }"); }
         }
 
         public static string MailSenderAddress {
-            get {
-                return
-                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:MailSenderAddress"]) ?
-                        "{ Mail Sender Address }" :
-                        ConfigurationManager.AppSettings["Reference:MailSenderAddress"];
-            }
+            get { return GetFromConfigOrDefault("Reference:MailSenderAddress", "{ Mail Sender Address }"); }
         }
 
         public static string MailSourceDomain {
-            get {
-                return
-                    String.IsNullOrWhiteSpace(ConfigurationManager.AppSettings["Reference:MailSourceDomain"]) ?
-                        "{ Mail Source Domain }" :
-                        ConfigurationManager.AppSettings["Reference:MailSourceDomain"];
-            }
+            get { return GetFromConfigOrDefault("Reference:MailSourceDomain", "{ Mail Source Domain }"); }
         }
 
     }
